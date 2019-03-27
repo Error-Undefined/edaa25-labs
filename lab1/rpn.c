9669a71c36f96dbacd3573ed3e1df1b53a6ec5e2
Otto <ottome@live.com>
2019-03-27 09:06:28 +0100

    Add skeleton for rpn in lab1

---
+++ lab1/rpn.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define STACK_DEPTH 10
+
+double stack[STACK_DEPTH]; // A double array representing the rpn stack
+int stack_ptr; // The stack pointer
+
+void push(double nbr)
+{
+	//TODO: implement as a function that pushes nbr on the stack
+}
+
+double pop()
+{
+	//TODO: implement as a function that pops the stack at the current stack pointer
+}
+
+double rpn(char c)
+{
+	//TODO: implement as main rpn calculator function
+}
+
+int main(int argc, char** argv)
+{
+	stack_ptr=0;
+}

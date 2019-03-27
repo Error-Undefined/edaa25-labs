35909ab6d69cdfb7df5372a994c05775352dece4
Otto <ottome@live.com>
2019-03-27 11:24:24 +0100

    Implement one character working rpn

--- lab1/rpn.c
+++ lab1/rpn.c
@@ -3,41 +3,66 @@
 
 #define STACK_DEPTH 10
 
-double stack[STACK_DEPTH]; // A double array representing the rpn stack
 int stack_ptr; // The stack pointer
+double stack[STACK_DEPTH]; // An int array representing the rpn stack
 
 void error(const char* err)
 {
-	printf("Error at %s\n", err);
+	printf("Error at: %s\n", err);
 }
 
-void push(int nbr)
+void push(double nbr)
 {
-	if(stack_ptr>9){
+	if(stack_ptr==STACK_DEPTH){
 		error("Stack depth reached");
 		return;
 	}
-	stack[stack_ptr]=nbr;
 	stack_ptr++;
+	stack[stack_ptr]=nbr;
 }
 
-int pop()
+double pop()
 {
 	if(stack_ptr==0){
 		error("Cannot pop an empty stack");
 		return 0;
 	}
+	double ret=stack[stack_ptr];
 	stack_ptr--;
-	return stack[stack_ptr+1];
+	return ret;
 }
 
-double rpn()
+void rpn()
 {
 	//TODO: implement as main rpn calculator function
+
+	int c;
+	int linecount = 1;
+	
+	while(1){
+		c=getchar();
+		if (c==EOF){
+			return;
+		} else if(c=='\n') {
+			printf("Line %d: %lf\n",linecount,pop());
+			linecount++;
+		} else if(isdigit(c)){
+			push(c-'0');
+		} else if(c=='+') {
+			push(pop()+pop());
+		} else if(c=='-'){
+			double a=pop();
+			push(pop()-a);
+		} else if(c=='*'){
+			push(pop()*pop());
+		} else if(c=='/'){
+			double a=pop();
+			push(pop()/a);
+		} 	
+	}
 }
 	
 int main(int argc, char** argv)
-{
-	stack_ptr=0;
+{	
 	rpn();
 }

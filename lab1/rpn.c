eaeea650853114d187ede9d05bd3710083839693
Otto <ottome@live.com>
2019-03-28 12:17:47 +0100

    Add working rpn

--- lab1/rpn.c
+++ lab1/rpn.c
@@ -1,121 +1,205 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
 
 #define STACK_DEPTH 10
 
+
+int line; // Current line number
 int stack_ptr; // The stack pointer
-double stack[STACK_DEPTH]; // An int array representing the rpn stack
+double stack[STACK_DEPTH]; // An int array representing the rpn stack. It grows downwards.
 
-void error(const char* err, int n)
+void fail(const char* error, int c)
 {
-	printf("Line %d: error at: %s\n",n , err);
-	//printf("Stack pointer was %d\n", stack_ptr);
-}
-
-void push(double nbr)
-{
-	stack_ptr++;
-	stack[stack_ptr]=nbr;
-	printf("Pushed %lf, current ptr is %d\n",nbr,stack_ptr);
-}
-
-double pop()
-{
-	double ret=stack[stack_ptr];
-	stack_ptr--;
-	printf("Popped %lf, current ptr is %d\n",ret, stack_ptr);
-	return ret;
-}
-
-bool rpnLine(int line)
-{
-	//TODO: almost works but segfaults 
-
-	int c;
-	double current=0;
-	int prev;
-	
-	while(1){
-		prev=c;
+	printf("Line %d: error at %s with character %c\n", line, error, c);
+	while(c!='\n'){
 		c=getchar();
-
-		if(isdigit(prev)&&(c=='+'||c=='-'||c=='*'||c=='/')){
-			push(prev-'0');
-			if(c=='\n'){
-				return false;	
-			}	
-		}
-
-		if(c==' '){ 
-			if(stack_ptr==9){
-				error("Stack full", line);
-				return false;
-			}
-			if(isdigit(prev)){
-				push(current);
-			}
-			current=0;
-		} else if(isdigit(c)){
-			current*=10;
-			current+=(c-'0');
-		} else if(c=='+'){
-			if(stack_ptr<1){
-				error("Not enough numbers on stack (+)", line);
-				return false;
-			}
-			push(pop()+pop());
-		} else if(c=='-'){
-			if(stack_ptr<1){
-                                error("Not enough numbers on stack (-)", line);
-                                return false;
-                        }
-			double a=pop();
-			push(pop()-a);
-                } else if(c=='*'){
-			if(stack_ptr<1){
-                                error("Not enough numbers on stack (*)", line);
-                                return false;
-                        }
-			push(pop()*pop());
-                } else if(c=='/'){
-			if(stack_ptr<1){
-                                error("Not enough numbers on stack (/)", line);
-                                return false;
-                        }
-			double a=pop();
-			if(a==0){
-				error("Cannot divide by 0", line);
-				return false;
-			}
-			push(pop()/a);
-                } else if(c=='\n'){
-
-                        if(stack_ptr!=0){
-                                error("\\n",line);
-                        }
-                        return false;
-                } else if (c==EOF) {
-			return true;
-		} else {
-			error("Bad character", line);
-		}
 	}
 }
-	
-int main(int argc, char** argv)
-{	
-	int linecount=1;
+
+void success(double result)
+{
+	if(result-(int) result==0){
+		printf("Line %d: %d\n",line,(int) result);
+	} else {
+		printf("Line %d: %lf\n",line,result);
+	}
+}
+
+void push(double nbr, int c)
+{
+	stack_ptr--;
+	stack[stack_ptr]=nbr;
+	printf("Pushed %lf, stack_ptr is %d\n",nbr, stack_ptr);
+}
+
+double pop(int c)
+{
+	double a=stack[stack_ptr];
+	stack_ptr++;
+	printf("Popped %lf, stack_ptr is %d\n",a, stack_ptr);
+	return a;
+}
+
+bool canpush(int c)
+{
+	if(stack_ptr==0){
+		fail("Stack depth reached", c);
+		return 0;
+	}
+	return 1;
+}
+
+bool canpop(int c)
+{
+	if(stack_ptr==10){
+		fail("Tried to pop empty stack", c);
+		return 0;
+	}
+	return 1;
+}
+
+void rpnLine()
+{
+	int current = 0;
+	int c = 0;
+
 	while(1){
-		stack_ptr=-1;
-		if(rpnLine(linecount)){
-			break;
+		c=getchar();
+			
+		while(isdigit(c)){
+				current*=10;
+				current+=(c-'0');
+				c=getchar();
+				if(!isdigit(c)){
+					if(!canpush(c)){
+						return;
+					}
+					push(current, c);
+					current=0;
+				}
 		}
-		if(stack[0]-(int) stack[0]==0){
-			printf("Line %d: %d\n",linecount , (int) stack[0]);
+			
+		if(c==EOF){
+			exit(0);
+		}
+		
+		if(c=='\n'){
+			if(stack_ptr==9){
+				success(pop(c));
+				return;
+			} else {
+				fail("\\n", c);
+				return;
+			}
+		}
+		
+		if(c=='+'){
+			if(!canpop(c)){
+				return;
+			}
+			int a=pop(c);
+			
+			if(!canpop(c)){
+				return;
+			}
+			int b=pop(c);
+			
+			current=a+b;
+			
+			push(current, c);
+			if(!canpush(c)){
+				return;
+			}
+			
+			current=0;
+			
+		} else if(c=='-'){
+			
+			if(!canpop(c)){
+				return;
+			}
+			int a=pop(c);
+			
+			if(!canpop(c)){
+				return;
+			}
+			int b=pop(c);
+			
+			current=b-a;
+			
+			push(current, c);
+			if(!canpush(c)){
+				return;
+			}
+			
+			current=0;
+		} else if(c=='*'){
+			
+			if(!canpop(c)){
+				return;
+			}
+			int a=pop(c);
+			
+			if(!canpop(c)){
+				return;
+			}
+			int b=pop(c);
+			
+			current=a*b;
+			
+			push(current, c);
+			if(!canpush(c)){
+				return;
+			}
+			
+			current=0;
+		} else if(c=='/'){
+			
+			if(!canpop(c)){
+				return;
+			}
+			int a=pop(c);
+			
+			if(!canpop(c)){
+				return;
+			}
+			int b=pop(c);
+			
+			if(a==0){
+				fail("Division by 0", c);
+				return;
+			} 
+			current=b/a;
+			
+			push(current, c);
+			if(!canpush(c)){
+				return;
+			}
+			
+			if(stack_ptr==0){
+				fail("Stack depth reached", c);
+				return;
+			}
+			current=0;
+		} else if(isspace(c)) {
+		
 		} else {
-			printf("Line %d: %lf\n",linecount , stack[0]);	
-		}
-		linecount++;
+			fail("Invalid character", c);
+			return;
+		}		
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	line=1;
+	
+	while(1){	
+		stack_ptr=10;
+		rpnLine();
+		line++;
 	}
 }
